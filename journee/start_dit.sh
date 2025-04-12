8a976f4687ebb043eec0d68988e55ed1b381954e
zhanghan <hzhang9617@gmail.com>
2025-04-12 21:55:36 +0800

    add stage4

--- journee/start_dit.sh
+++ journee/start_dit.sh
@@ -8,7 +8,7 @@ echo $PYTHONPATH
 # export HF_HOME="/mnt/world_model/longxiang/.cache/huggingface"
 
 export NCCL_BUFFSIZE=1048576  # for 24 GB memory, the default 32MB NCCL buffer per channel would be too large and cause OOM (These memory wouldn't be released by `torch.cuda.empty_cache()`)
-CUDA_VISIBLE_DEVICES=0; torchrun --nnodes 1 --nproc-per-node 1 --master-port 29501 ./stage3_ray/inference_ulysses_interactive.py \
+CUDA_VISIBLE_DEVICES=0; torchrun --nnodes 1 --nproc-per-node 1 --master-port 29501 ./stage4_ray/inference_ulysses_interactive.py \
 --model_path "/MODEL/PATH" \
 --output_path ../samples/journee/dit_debug/output.mp4 \
 --prompt "The video shows a white car driving on a country road on a sunny day. The car comes from the back of the scene, moving forward along the road, with open fields and distant hills surrounding it. As the car moves, the vegetation on both sides of the road and distant buildings can be seen. The entire video records the car's journey through the natural environment using a follow-shot technique." \
